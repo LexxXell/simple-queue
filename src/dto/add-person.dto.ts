6e1ce904ddcf4e636d2299ad1c2e6c38e8e6bd4c
LexxXell <lexxxell007@gmail.com>
2023-11-17 14:55:22 +0400

    Fixed relations between Person and AddPersonDto

--- src/dto/add-person.dto.ts
+++ src/dto/add-person.dto.ts
@@ -1,4 +1,31 @@
-export interface AddPersonDto {
+/**
+ * Data Transfer Object (DTO) interface for adding a person to the queue.
+ *
+ * @remarks
+ * - This interface extends the 'Person' model and specifies additional properties for adding a person.
+ * - The 'name' and 'surname' properties are required for creating a new person entry.
+ *
+ * @see {@link ../models/Person.ts}
+ *
+ * @example
+ * ```typescript
+ * import { AddPersonDto } from '../dto';
+ *
+ * // Example usage of the 'AddPersonDto' interface
+ * const newPerson: AddPersonDto = {
+ *   name: 'John',
+ *   surname: 'Doe',
+ *   // Additional optional properties from the 'Person' model can be included here
+ * };
+ * ```
+ */
+import { Person } from '../models';
+
+// Define the AddPersonDto interface, extending the 'Person' model
+export interface AddPersonDto extends Partial<Person> {
+  // Required property: the name of the person
   name: string;
+
+  // Required property: the surname of the person
   surname: string;
 }
--- src/models/person.model.ts
+++ src/models/person.model.ts
@@ -1,5 +1,31 @@
-import { AddPersonDto } from '../dto';
+/**
+ * Model interface representing a person in the queue.
+ *
+ * @remarks
+ * - This interface defines the properties of a person, including their 'name', 'surname', and 'timestamp'.
+ * - The 'name' property represents the first name of the person.
+ * - The 'surname' property represents the last name or surname of the person.
+ * - The 'timestamp' property represents the date and time when the person was added to the queue.
+ *
+ * @example
+ * ```typescript
+ * import { Person } from '../models';
+ *
+ * // Example usage of the 'Person' interface
+ * const person: Person = {
+ *   name: 'John',
+ *   surname: 'Doe',
+ *   timestamp: 1637032856000, // Example timestamp in milliseconds
+ * };
+ * ```
+ */
+export interface Person {
+  // Property representing the first name of the person
+  name: string;
 
-export interface Person extends AddPersonDto {
+  // Property representing the last name or surname of the person
+  surname: string;
+
+  // Property representing the date and time when the person was added to the queue (in milliseconds)
   timestamp: number;
 }
